92c39f77686e1936c990caa18078b3b1b1ad5b97
Sconstante <sconstante@uninorte.edu.co>
2024-05-22 13:15:34 -0500

    falabella listo

--- index.js
+++ index.js
@@ -17,16 +17,11 @@ app.post("/product", async (req, res) => {
   products = [];
   const productName = req.body.product;
 
-  const [productsMercadoLibre, productsAlkosto, productsExito] =
-    await Promise.all([
-      searchProductMercadoLibre(productName),
-      searchProductAlkosto(productName),
-      searchProductExito(productName),
-    ]);
+  const [productsFalabella] = await Promise.all([
+    searchProductFalabella(productName),
+  ]);
 
-  products.push(...productsMercadoLibre);
-  products.push(...productsAlkosto);
-  products.push(...productsExito);
+  products.push(...productsFalabella);
 
   //const productsOlimpica = await searchProductOlimpica(productName);
   //const productsExito = await searchProductExito(productName);
@@ -421,6 +416,131 @@ const searchProductExito = async (product) => {
   //Create the JSON object of the products
   const products = createJSONObject(
     "Exito",
+    productsName,
+    prices,
+    productsImg,
+    productsDescription,
+    productsLink,
+    productsName.length
+  );
+  return products;
+};
+
+const searchProductFalabella = async (product) => {
+  console.log("inicio Falabella");
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.falabella.com.co/falabella-co");
+  await page.waitForLoadState("domcontentloaded");
+
+  // Write the product in the search bar
+  await page.click("#testId-SearchBar-Input");
+  await page.keyboard.type(product);
+
+  // Simulate pressing the Enter key
+  await page.keyboard.press("Enter");
+  await page.waitForLoadState("networkidle");
+  await page.screenshot({ path: "ssFalabella.png" });
+
+  const productsName = [];
+  const prices = [];
+  const productsImg = [];
+  const productsLink = [];
+  const productsDescription = [];
+
+  const numberOfElements = await page.$$('b[id^="testId-pod-displaySubTitle"]');
+
+  for (let i = 0; i < numberOfElements.length; i++) {
+    await page.screenshot({ path: "ssfalabella.png" });
+    console.log("Screenshot tomada");
+    await page.waitForSelector('b[id^="testId-pod-displaySubTitle"]');
+
+    //Wait to get the elements title
+    const elementsName = await page.$$('b[id^="testId-pod-displaySubTitle"]');
+
+    const elementsPrice = await page.$$(
+      ".copy10.primary.medium.jsx-3451706699.normal.line-height-22"
+    );
+
+    const elementsName_Link = await page.$$(".jsx-1484439449");
+
+    const elementsImg = await page.$$('div[class^="jsx-2469003054"]');
+
+    const elementsDescription = await page.$$(
+      ".jsx-4018082099.section__pod-bottom-description"
+    );
+
+    console.log(elementsDescription);
+
+    //Get the title of the product
+    let title = await elementsName[i].evaluate((tit) => tit.textContent.trim());
+    title = title.split("|")[0];
+
+    //Check if the title of the element includes the product the user wants
+    if (verifyNameProduct(product, title)) {
+      productsName.push(title);
+      console.log(title);
+
+      //Get the link
+      const link = await elementsName_Link[i].evaluate(
+        (link) => link.children[0].href
+      );
+      productsLink.push(link);
+      console.log(link);
+
+      // Get the price
+      let price = await elementsPrice[i].evaluate((price) =>
+        price.textContent.trim()
+      );
+      price = price.split("-")[0];
+      prices.push(price);
+      console.log(price);
+
+      //Get the img src
+      const src = await elementsImg[i].evaluate(
+        (img) => img.children[0].children[0].children[1].src
+      );
+      console.log(src);
+      productsImg.push(src);
+
+
+      let descriptions = [];
+      if (elementsDescription.length > 0) {
+        
+        const descriptionItems = await elementsDescription[i].$$("li");
+        if (descriptionItems.length > 0) {
+          descriptions = await Promise.all(
+            descriptionItems.map((item) =>
+              item.evaluate((el) => el.textContent.trim())
+            )
+          );
+        } else {
+          descriptions.push("Más información del producto en el enlace"); // Agrega esta línea si no hay elementos de descripción
+        }
+      } else {
+        descriptions.push("Más información del producto en el enlace"); // Agrega esta línea si no hay elementos de descripción
+      }
+
+      productsDescription.push(descriptions);
+      console.log(descriptions);
+
+      //Get only the first 5 products
+      if (productsName.length === 5) {
+        break;
+      }
+    }
+  }
+
+  // Take a screenshot of the page
+  //NOTE: This is just for testing purposes
+  await page.screenshot({ path: "ssfalabella.png" });
+
+  await browser.close();
+  console.log("Fin Falabella");
+
+  //Create the JSON object of the products
+  const products = createJSONObject(
+    "Falabella",
     productsName,
     prices,
     productsImg,
